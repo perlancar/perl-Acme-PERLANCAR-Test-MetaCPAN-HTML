dc16625a92bb41f9edbd435a2158eaba29aea7c4
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-22 19:44:34 +0700

    Release v0.001

--- lib/Acme/PERLANCAR/Test/MetaCPAN/HTML/Inline1.pm
+++ lib/Acme/PERLANCAR/Test/MetaCPAN/HTML/Inline1.pm
@@ -6,6 +6,8 @@ package Acme::PERLANCAR::Test::MetaCPAN::HTML::Inline1;
 1;
 # ABSTRACT: Test inline HTML
 
-=for html <!--begin comment--><table><tr><td>1</td><td>2</td></tr></table><!--end comment>
+=for html <!-- begin comment --><table><tr><td>1</td><td>2</td></tr></table><!-- end comment -->
 
-=for html <!--begin comment--><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" /><!--end comment>
+=for html <!-- begin comment --><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" /></script><!-- end comment -->
+
+=for html <!-- begin comment --><script>alert("hello");</script><!-- end comment -->
