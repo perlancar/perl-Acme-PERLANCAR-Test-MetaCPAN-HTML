899c776a1779d9d2a4dfe4b566fd0faab209f8b9
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-22 20:10:07 +0700

    Add more HTML

--- lib/Acme/PERLANCAR/Test/MetaCPAN/HTML/Inline1.pm
+++ lib/Acme/PERLANCAR/Test/MetaCPAN/HTML/Inline1.pm
@@ -6,7 +6,7 @@ package Acme::PERLANCAR::Test::MetaCPAN::HTML::Inline1;
 1;
 # ABSTRACT: Test inline HTML
 
-=for html <!-- begin comment --><table><tr><td>1</td><td>2</td></tr></table><!-- end comment -->
+=for html <!-- begin comment --><table border><tr><td>1</td><td>2</td></tr></table><!-- end comment -->
 
 =for html <!-- begin comment --><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" /></script><!-- end comment -->
 
@@ -16,4 +16,27 @@ package Acme::PERLANCAR::Test::MetaCPAN::HTML::Inline1;
 
 =for html <!-- begin comment --><style> </style><!-- end comment -->
 
-=for html <div id=#one> </div>
+=for html <div id=#one> inside div </div>
+
+=for html <div class=one> inside div2 </div>
+
+=for html <pre class="line-numbers">foo</pre>
+
+=begin html
+
+<pre class="line-numbers">line 1
+line 2
+line 3
+</pre>
+
+=end html
+
+=head1 NOTES
+
+Tables allowed.
+
+HTML comments stripped.
+
+<script>, <style> stripped.
+
+<div> allowed but attributes stripped.
