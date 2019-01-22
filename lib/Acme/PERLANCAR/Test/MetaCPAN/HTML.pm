0a62565ee83c3ec487f92a1d46dc987293409f3d
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-22 19:41:32 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/MetaCPAN/HTML.pm
@@ -0,0 +1,7 @@
+package Acme::PERLANCAR::Test::MetaCPAN::HTML;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Test HTML rendering on MetaCPAN
---
+++ lib/Acme/PERLANCAR/Test/MetaCPAN/HTML/Inline1.pm
@@ -0,0 +1,11 @@
+package Acme::PERLANCAR::Test::MetaCPAN::HTML::Inline1;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Test inline HTML
+
+=for html <!--begin comment--><table><tr><td>1</td><td>2</td></tr></table><!--end comment>
+
+=for html <!--begin comment--><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" /><!--end comment>
